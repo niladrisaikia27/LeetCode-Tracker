c639c8a095ecbe9f497bc0b3dc9ff4649217176a
Aoi Yama <niladrisaikia@gmail.com>
2026-01-11 00:34:16 +0530

    Time: 0 ms (100%), Space: 8.6 MB (40.26%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int dup = x;
+        int sum =0;
+        if(x < 0)
+            return false;
+        while(dup!=0){
+            int last = dup % 10;
+            dup = dup /10;
+
+            if(sum > INT_MAX/10 || sum == INT_MAX/10 && last > 7)
+                return 0;
+            if(sum < INT_MIN/10 || sum == INT_MIN/10 && last < -8)
+                return 0;
+
+            sum = (sum*10) + last ;
+        }
+        if(sum == x)
+            return 1;
+        else    return 0;
+    }
+};

926c4a8127bac0f5ee56b0f66ece03427ffc185a
Aoi Yama <niladrisaikia@gmail.com>
2026-01-11 00:36:14 +0530

    Time: 0 ms (100%), Space: 8.6 MB (40.26%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -11,8 +11,8 @@ public:
 
             if(sum > INT_MAX/10 || sum == INT_MAX/10 && last > 7)
                 return 0;
-            if(sum < INT_MIN/10 || sum == INT_MIN/10 && last < -8)
-                return 0;
+            //if(sum < INT_MIN/10 || sum == INT_MIN/10 && last < -8)
+              //  return 0;
 
             sum = (sum*10) + last ;
         }

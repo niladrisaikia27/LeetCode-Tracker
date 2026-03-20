f3c5bb1a4c65aa7438b858c6e8ac1ac13706d08b
Aoi Yama <niladrisaikia@gmail.com>
2026-03-20 23:34:09 +0530

    Time: 0 ms (100%), Space: 11.6 MB (48.46%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int count0 = 0, count1 = 0, count2 = 0;
+
+        // First pass: Count the frequencies
+        for (int num : nums) {
+            if (num == 0) count0++;
+            else if (num == 1) count1++;
+            else count2++;
+        }
+
+        // Second pass: Overwrite the array
+        int i = 0;
+        while (count0 > 0) {
+            nums[i++] = 0;
+            count0--;
+        }
+        while (count1 > 0) {
+            nums[i++] = 1;
+            count1--;
+        }
+        while (count2 > 0) {
+            nums[i++] = 2;
+            count2--;
+        }
+    }
+};

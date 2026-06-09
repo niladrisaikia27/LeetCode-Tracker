e600016c17510baa659e997cb1e3a7f49fff2401
Aoi Yama <niladrisaikia@gmail.com>
2026-06-09 22:36:18 +0530

    Time: 0 ms (100%), Space: 71.6 MB (79.93%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int max_sum = nums[0];
-        int current_sum = 0;
+        int max_so_far = nums[0];
+        int current_sum = nums[0];
         
-        for (int x : nums) {
-            current_sum += x;
-            
-            if (current_sum > max_sum) {
-                max_sum = current_sum;
-            }
-            
-            if (current_sum < 0) {
-                current_sum = 0;
-            }
+        for(int i=1; i<nums.size(); i++){
+            current_sum = max(nums[i], current_sum + nums[i]);
+            max_so_far = max(current_sum, max_so_far);
         }
-        
-        return max_sum;
+        return max_so_far;
     }
 };

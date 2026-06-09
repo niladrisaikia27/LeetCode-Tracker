70b9663eb4e66b9361db2a4c35444753bab20b1f
Aoi Yama <niladrisaikia@gmail.com>
2026-06-09 22:33:52 +0530

    Time: 0 ms (100%), Space: 103.8 MB (32.68%) - LeetHub

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.cpp
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     long long maxTotalValue(vector<int>& nums, int k) {
-        auto [min_it, max_it] = std::minmax_element(nums.begin(), nums.end());
+        auto [min_it, max_it] = minmax_element(nums.begin(), nums.end());
         long long max_diff = *max_it - *min_it;
         
         return max_diff * k;

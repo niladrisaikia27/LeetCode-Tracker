4fab318588b13d1e1cb95a8ec34f7fa16d09049c
Aoi Yama <niladrisaikia@gmail.com>
2026-06-11 23:11:42 +0530

    Time: 0 ms (100%), Space: 128.4 MB (54.44%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -8,7 +8,7 @@ public:
         int negIndex = 1;
         
         for (int i = 0; i < n; i++) {
-            if (nums[i] > 0) {
+            if (nums[i] >= 0) {
                 ans[posIndex] = nums[i];
                 posIndex += 2;
             } else {

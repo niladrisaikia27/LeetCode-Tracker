6107bc972b9c4f04c4a6c768e0969ec8beee3367
Aoi Yama <niladrisaikia@gmail.com>
2026-06-11 17:05:51 +0530

    Time: 0 ms (100%), Space: 20.1 MB (60.61%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -5,7 +5,7 @@ public:
 
         for(int i=1; i<prices.size(); i++){
             if(prices[i] > prices[i-1]){
-                max_profit = max(max_profit, max_profit + (prices[i] - prices[i-1]));
+                max_profit += prices[i] - prices[i-1];
             }
         }
         return max_profit;

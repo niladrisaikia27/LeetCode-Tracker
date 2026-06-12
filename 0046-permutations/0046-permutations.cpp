f73f0435507ad260c27482160543642bedcf8bc2
Aoi Yama <niladrisaikia@gmail.com>
2026-06-13 00:55:40 +0530

    Time: 3 ms (26.41%), Space: 10.6 MB (48.81%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        recursivePermute(0, nums, ans);
+        return ans;
+    }
+
+private:
+    void recursivePermute(int index, vector<int>& nums, vector<vector<int>>& ans){
+        if(index == nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=index; i<nums.size(); i++){
+            swap(nums[index], nums[i]);
+            recursivePermute(index+1, nums, ans);
+            swap(nums[index], nums[i]);
+        }
+    }
+};

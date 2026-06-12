8ac16ac9f95098e9ef6e41104ae389886cb589e7
Aoi Yama <niladrisaikia@gmail.com>
2026-06-13 01:20:34 +0530

    Time: 10 ms (21.09%), Space: 15.2 MB (11.85%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    void recursivePermute(int index, vector<int>& nums, vector<vector<int>>& ans){
+        if(index == nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        unordered_set<int> freq;
+        for(int i=index; i<nums.size(); i++){
+            if(freq.count(nums[i])){ 
+                continue;
+            }
+            freq.insert(nums[i]);
+
+            swap(nums[index], nums[i]);
+            recursivePermute(index+1, nums, ans);
+            swap(nums[index], nums[i]);
+        }
+    }
+
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        recursivePermute(0, nums, ans);
+        return ans;
+    }
+};

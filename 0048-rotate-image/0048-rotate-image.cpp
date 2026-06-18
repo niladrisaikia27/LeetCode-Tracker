fe3d7c9ef6d931f7f6c11cb241db531632f1dbdf
Aoi Yama <niladrisaikia@gmail.com>
2026-06-19 00:18:39 +0530

    Time: 0 ms (100%), Space: 10.3 MB (34.39%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,16 +4,12 @@ public:
         int n = matrix.size();
         
         for(int i=0; i<n; i++){
-            int j = i;
-            while(j<n){
+            for(int j=i; j<n; j++){
                 swap(matrix[i][j], matrix[j][i]);
-                j++;
             }
         }
         for(int i=0; i<n; i++){
-            for(int j=0; j<n/2; j++){
-                swap(matrix[i][j], matrix[i][n-1-j]);
-            }            
+            reverse(matrix[i].begin(), matrix[i].end());            
         }
     }
 };

2d3c712aa983c62238c9ed178de74dbc7455cdff
Aoi Yama <niladrisaikia@gmail.com>
2026-06-19 00:02:56 +0530

    Time: 0 ms (100%), Space: 10.3 MB (34.39%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-
-        vector<vector<int>> copyMatrix(m, vector<int>(m, 0));
-
-        for(int i=0; i<m; i++){
-            int k = m-1;
-            for(int j=0; j<m; j++){
-                copyMatrix[i][j] = matrix[k][i];
-                k--;
+        int n = matrix.size();
+        
+        for(int i=0; i<n; i++){
+            int j = i;
+            while(j<n){
+                swap(matrix[i][j], matrix[j][i]);
+                j++;
             }
         }
-        matrix = copyMatrix;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n/2; j++){
+                swap(matrix[i][j], matrix[i][n-1-j]);
+            }            
+        }
     }
 };

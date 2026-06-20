598dfa5148e04cd52babe1a438f65c8c8d080a8d
Aoi Yama <niladrisaikia@gmail.com>
2026-06-20 23:39:40 +0530

    Time: 0 ms (100%), Space: 23.5 MB (82.03%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,19 +1,48 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> count;
-        vector<int> res;
-        int n = nums.size();
-        //count frequencies of each element
+        int candidate1 = 0, candidate2 = 0;
+        int count1 = 0, count2 = 0;
+
+        //find the candidates
         for(int num : nums){
-            count[num]++;
-        }
-        //return elements that appear more than n/3 times
-        for(auto& x : count){
-            if(x.second > n/3){
-                res.push_back(x.first);
+            if(num == candidate1){
+                count1++;
+            }
+            else if(num == candidate2){
+                count2++;
+            }
+            else if(count1 == 0){
+                candidate1 = num;
+                count1 = 1;
+            }
+            else if(count2 == 0){
+                candidate2 = num;
+                count2 = 1;
+            }
+            else{
+                count1--;
+                count2--;
             }
         }
-        return res;
+
+        //check out count of both candidates
+        count1 = 0;
+        count2 = 0;
+        for(int num : nums){
+            if(num == candidate1){
+                count1++;
+            }
+            else if(num == candidate2){
+                count2++;
+            }
+        }
+
+        int n = nums.size();
+        vector<int> ans;
+        if(count1 > n/3)    ans.push_back(candidate1);
+        if(count2 > n/3)    ans.push_back(candidate2);
+        
+        return ans;
     }
 };

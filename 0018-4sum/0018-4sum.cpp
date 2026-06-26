2c5927d370cb5b06a8500c238ae3d50ec53e0258
Aoi Yama <niladrisaikia@gmail.com>
2026-06-27 00:40:07 +0530

    Time: 3 ms (97.79%), Space: 17.4 MB (54.49%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -7,11 +7,17 @@ public:
 
         sort(nums.begin(), nums.end());
 
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n-3; i++){
             if(i>0 && nums[i] == nums[i-1]) continue;
+            
+            if((long long)nums[i] + nums[i+1] + nums[i+2] + nums[i+3] > target) break;
+            if((long long)nums[i] + nums[n-1] + nums[n-2] + nums[n-3] < target) continue;
 
-            for(int j=i+1; j<n; j++){
+            for(int j=i+1; j<n-2; j++){
                 if(j>i+1 && nums[j] == nums[j-1])   continue;
+
+                if((long long)nums[i] + nums[j] + nums[j+1] + nums[j+2] > target) break;
+                if((long long)nums[i] + nums[j] + nums[n-1] + nums[n-2] < target) continue;
 
                 int k = j+1;
                 int l = n-1;

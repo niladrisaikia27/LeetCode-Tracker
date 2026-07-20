57216b1a986f9f6eb8f37dab9b8147a591899091
Aoi Yama <niladrisaikia@gmail.com>
2026-07-20 14:53:50 +0530

    Time: 7 ms (38.22%), Space: 23.7 MB (82.43%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,27 +2,18 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         if (intervals.empty()) return {};
-        bool mergedAny = true;
+        
+        vector<vector<int>> merged;
+        sort(intervals.begin(), intervals.end());
 
-        while(mergedAny){
-            mergedAny = false;
-            for(int i=0; i<intervals.size(); i++){
-                for(int j=i+1; j<intervals.size(); j++){
-                    //check if first element of j is smaller than second element of i
-                    if(max(intervals[i][0], intervals[j][0]) <= min(intervals[i][1], intervals[j][1])){
-                        //merge the intervals into i
-                        intervals[i][0] = min(intervals[i][0], intervals[j][0]);
-                        intervals[i][1] = max(intervals[i][1], intervals[j][1]);
-
-                        //delete the j interval
-                        intervals.erase(intervals.begin() + j);
-                        mergedAny =  true;
-                        break;                  
-                    }
-                }
-                if(mergedAny)   break;
+        for(const auto& interval : intervals){
+            if(merged.empty() || merged.back()[1] < interval[0]){
+                merged.push_back(interval);
+            }
+            else{
+                merged.back()[1] = max(merged.back()[1], interval[1]);
             }
         }
-        return intervals;
+        return merged;
     }
 };

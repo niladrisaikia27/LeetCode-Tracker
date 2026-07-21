c9c2cce669852a13e01e0c6fcf30d70ae89cb98f
Aoi Yama <niladrisaikia@gmail.com>
2026-07-21 20:57:07 +0530

    Time: 0 ms (100%), Space: 24.1 MB (19.54%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,17 +3,47 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         if (intervals.empty()) return {};
         
-        vector<vector<int>> merged;
-        sort(intervals.begin(), intervals.end());
+        //find maximum start in the interval
+        int max_val = 0;
+        for(const auto& start : intervals){
+            max_val = max(max_val, start[0]);
+        }
 
-        for(const auto& interval : intervals){
-            if(merged.empty() || merged.back()[1] < interval[0]){
-                merged.push_back(interval);
+        //create a vector/array to store max end where index represents start and value represents max end reached from that start
+        vector<int> max_end(max_val + 1, -1);
+        for(const auto& start : intervals){
+            max_end[start[0]] = max(max_end[start[0]], start[1]);
+        }
+
+        vector<vector<int>> merged;
+        int current_start = -1;
+        int current_end = -1;
+
+        //loop through max_end , update the result and insert it into merged
+        for(int i=0; i<=max_val; i++){
+            if(max_end[i] == -1)    continue;
+
+            //if it is the starting index
+            if(current_start == -1){
+                current_start = i;
+                current_end = max_end[i];
             }
+
+            else if(i <= current_end){
+                current_end = max(current_end, max_end[i]);
+            }
+
             else{
-                merged.back()[1] = max(merged.back()[1], interval[1]);
+                merged.push_back({current_start, current_end});
+                current_start = i;
+                current_end = max_end[i];
             }
         }
+
+        if(current_start != -1){
+            merged.push_back({current_start, current_end});
+        }
+
         return merged;
     }
 };

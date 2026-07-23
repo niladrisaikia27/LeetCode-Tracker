2935c329fb523f899a7ad915f520be02f213b992
Aoi Yama <niladrisaikia@gmail.com>
2026-07-23 17:30:37 +0530

    Time: 0 ms (100%), Space: 12.6 MB (6.2%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i=0; i<n; i++){
-            nums1[m+i] = nums2[i];
+        vector<int> num1_copy(nums1.begin(), nums1.begin()+m);
+
+        int p1=0, p2=0;
+        for(int i=0; i<m+n; i++){
+            if(p2>=n || (p1<m && num1_copy[p1] < nums2[p2])){
+                nums1[i] = num1_copy[p1];
+                p1++;
+            }
+            else{
+                nums1[i] = nums2[p2];
+                p2++;
+            }
         }
-        sort(nums1.begin(), nums1.end());
-        }
+    }
 };

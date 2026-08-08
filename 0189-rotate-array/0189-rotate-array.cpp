7421ac7a81408c76747f782bee9c9b34cd5c378d
Aoi Yama <niladrisaikia@gmail.com>
2026-08-08 19:33:46 +0530

    Time: 0 ms (100%), Space: 29.7 MB (28.94%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,9 +3,9 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k %= n;
-        if (k == 0) return;
+        if(k==0)    return;
         reverse(nums.begin(), nums.end());
-        reverse(nums.begin(), nums.begin() + k);
-        reverse(nums.begin() + k, nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
     }
 };

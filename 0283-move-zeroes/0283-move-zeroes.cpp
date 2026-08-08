41693fab7f56b81dace383edc907b0039beb95f2
Aoi Yama <niladrisaikia@gmail.com>
2026-08-08 22:54:05 +0530

    Time: 0 ms (100%), Space: 23.8 MB (56.71%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,16 +2,11 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        vector<int> temp(n, 0);
-        int k = 0;
-        for(int i=0; i<n; i++){
-            if(nums[i]!=0){
-                temp[k] = nums[i];
-                k++;
+        for(int left=0, curr=0; curr<n; curr++){
+            if(nums[curr]!=0){
+                swap(nums[left], nums[curr]);
+                left++;
             }
-        }
-        for(int j=0; j<n; j++){
-            nums[j] = temp[j];
         }
     }
 };

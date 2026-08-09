c471fc1c8c5251eb79abc9df34f3a39b72539471
Aoi Yama <niladrisaikia@gmail.com>
2026-08-09 11:34:57 +0530

    Time: 0 ms (100%), Space: 20.7 MB (59.41%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int result = 0;
-        for (int num : nums) {
-            result ^= num; 
+        for(int num : nums){
+            result ^= num;
         }
         return result;
     }

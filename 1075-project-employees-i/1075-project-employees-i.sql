f9b451ef1a8dab77ad69120843fbfa791f97c430
Aoi Yama <niladrisaikia@gmail.com>
2026-08-29 00:29:37 +0530

    Time: 528 ms (74.5%), Space: 0B (100%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
 SELECT p.project_id, 
-    ROUND(AVG(experience_years),
-    2) AS average_years
+    ROUND(AVG(e.experience_years), 2) AS average_years
 FROM Project p
 LEFT JOIN
 Employee e ON p.employee_id = e.employee_id

28879677a6a9f6416774bc79a6ffe0d2a6c1a0ed
Aoi Yama <niladrisaikia@gmail.com>
2026-08-28 23:08:24 +0530

    Time: 342 ms (16.87%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 SELECT id, movie, description, rating FROM Cinema
-WHERE MOD(id, 2) = 1 AND description <> 'boring'
+WHERE id & 1 = 1 AND description <> 'boring'
 ORDER BY rating DESC;

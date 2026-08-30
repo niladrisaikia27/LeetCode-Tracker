4fe84162d6d947ba752a690e479979159096d579
Aoi Yama <niladrisaikia@gmail.com>
2026-08-30 15:44:26 +0530

    Time: 532 ms (91.28%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -4,5 +4,5 @@ WHERE primary_flag = 'Y'
     OR employee_id IN(
         SELECT employee_id FROM Employee
         GROUP BY employee_id
-        Having COUNT(*) = 1 AND primary_flag = 'N'
+        Having COUNT(department_id) = 1
     );

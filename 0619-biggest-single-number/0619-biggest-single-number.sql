0a06e3ebb08bcbc301b342095d48752cd0c69408
Aoi Yama <niladrisaikia@gmail.com>
2026-08-30 14:26:39 +0530

    Time: 455 ms (63.89%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,8 +1,7 @@
 # Write your MySQL query statement below
-SELECT (
+SELECT MAX(num) AS num FROM MyNumbers
+WHERE num IN(
     SELECT num FROM MyNumbers
     GROUP BY num
     HAVING COUNT(num) = 1
-    ORDER BY num DESC
-    LIMIT 1
-) AS num;
+);
